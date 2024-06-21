764fe458f96f721758684f0e57ad605335407b16
Thanh <trmthanh220895@gmail.com>
2024-06-21 22:54:42 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-sdfsdfd
+
+import express from 'express'
+import cors from 'cors'
+import rootRouter from './routes/index.js'
+import dotenv from 'dotenv'
+var app=express()
+dotenv.config();
+const port = process.env.PORT || 3001
+
+var  corsOptions  = {
+  origin:["https://thegioimauxanh.com","http://localhost:3000","https://peaceful-sunflower-941f16.netlify.app"], //frontend url
+  credentials: true,
+  allowedHeaders:'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
+  exposedHeaders:'Content-Range, X-Content-Range'
+}
+
+
+app.use(cors(corsOptions));
+
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
+
+app.use(rootRouter)
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ src/models/connect.js
@@ -0,0 +1,39 @@
+import { Sequelize } from 'sequelize'
+import config from '../config/config.js'
+
+
+// console.log(' db_database', config)
+
+const sequelize = new Sequelize(
+  config.db_database,
+  config.db_user,
+  config.db_password,
+  {
+      host: config.db_host,
+      port: config.db_port,
+      dialect: config.db_dialect,
+  }
+)
+
+// const sequelize = new Sequelize('restaurant', 'root', '159753', {
+//   host: 'localhost',
+//   port:"3310",
+//   dialect: "mysql"
+// });
+
+// npx sequelize auto -h localhost -d anh_data -u root -x 159753 -p 3310 --dialect mysql -o src/models -l esm
+// npx sequelize-auto -h localhost -d anh_data -u root -x 159753 -p 3310 --dialect mysql -o src/models -l esm
+
+
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+export default sequelize
+
+
+// npx sequelize-auto -h localhost -d data_chuan01 -u root -x 159753 -p 3310  --dialect mysql -o src/models -l esm 
